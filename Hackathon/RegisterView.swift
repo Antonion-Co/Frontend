065041b0b5aa9f7f7b5eebc3a738094ebd8f8452
Pierre Dubosq <pierre@dubosq.eu>
2023-05-05 14:09:49 -0600

    Add files via upload

--- Hackathon/RegisterView.swift
+++ Hackathon/RegisterView.swift
@@ -5,7 +5,6 @@
 //  Created by Alumno on 05/05/23.
 //
 
-import Alamofire
 import SwiftUI
 
 struct RegisterView: View {
@@ -53,10 +52,49 @@ struct RegisterView: View {
         .padding()
     }
 
-    func register() {
-        let username: string = ""
-        let password: string = ""
+    enum LoginError: Error {
+        case connectionError
+        case invalidResponse
+        case authenticationError
+        case invalidCredentials
+        case unknownError
+    }
 
-        guard let url 
+    func register(completionHandler: @escaping (Error?) -> Void) throws {
+        let username: String = ""
+        let password: String = ""
+
+        guard let url = URL(string: "https://example.com/api/login") else {
+            throw LoginError.connectionError
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        let params = ["username": username, "password": password]
+        request.httpBody = try JSONSerialization.data(withJSONObject: params, options: [])
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("Error : \(error.localizedDescription)")
+                completionHandler(LoginError.connectionError)
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completionHandler(LoginError.invalidResponse)
+                return
+            }
+            
+            if httpResponse.statusCode == 200 {
+                completionHandler(nil)
+            } else if httpResponse.statusCode == 401 {
+                completionHandler(LoginError.invalidCredentials)
+            } else {
+                completionHandler(LoginError.unknownError)
+            }
+        }
+        
+        task.resume()
     }
 }
